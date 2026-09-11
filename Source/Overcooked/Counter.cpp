da5a7360eb3dd165d2e5187776a20bf6a6180a95
melludy0331 <melludy0331@gmail.com>
2026-09-11 17:22:33 +0900

    fix: 픽업 드랍 수정

---
+++ Source/Overcooked/Counter.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Counter.h"
+
+
+// Sets default values
+ACounter::ACounter()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ACounter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACounter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/Overcooked/PlaceableInterface.cpp
@@ -0,0 +1,7 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlaceableInterface.h"
+
+
+// Add default functionality here for any IPlaceableInterface functions that are not pure virtual.
